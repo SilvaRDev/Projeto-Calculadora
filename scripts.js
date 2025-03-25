b03809ba8cd69b46cdf5140782a4acfb4cd3d7f7
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-03-25 10:35:03 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
+document.addEventListener("keydown", function(event) { /* Verifica se as teclas digitadas correspondem as teclas da calculadora */
+  const teclasPermitidas = [
+      "Numpad0", "Numpad1", "Numpad2", "Numpad3", "Numpad4", 
+      "Numpad5", "Numpad6", "Numpad7", "Numpad8", "Numpad9",
+      "Digit0", "Digit1", "Digit2", "Digit3", "Digit4",
+      "Digit5", "Digit6", "Digit7", "Digit8", "Digit9",
+      "NumpadDecimal", "NumpadAdd", "NumpadSubtract", 
+      "NumpadMultiply", "NumpadDivide", "NumpadEnter", "Enter", "Backspace"
+  ];
+
+  if (!teclasPermitidas.includes(event.code)) {
+      event.preventDefault();
+      console.log(`Tecla "${event.key}" bloqueada!`);
+  }
+});
+
+document.addEventListener('keydown', function(event) { /* Quando o usuário aperta enter, o calculo é feito. */
+  if (event.key === 'Enter') {
+    calcular()
+  }
+});
+
 function adicionarCaracter(caracter) {
   const valorInput = document.querySelector('.display').value
   document.querySelector('.display').value = valorInput + caracter
