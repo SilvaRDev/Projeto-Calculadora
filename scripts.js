76a3626e2b91ae65cd71f2e11f732ccf09fb5f77
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-03-25 09:48:41 -0300

    Adicionado scripts.js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function adicionarCaracter(caracter) {
+  const valorInput = document.querySelector('.display').value
+  document.querySelector('.display').value = valorInput + caracter
+}
+
+function limparTela() {
+  document.querySelector('.display').value = ''
+}
+
+function calcular() {
+  const valorInput = document.querySelector('.display').value
+
+  document.querySelector('.display').value = eval(valorInput)
+}
+
+function inverte() {
+  const valorInput = document.querySelector('.display').value
+
+  document.querySelector('.display').value = valorInput * -1
+}
+
+function aoClicarNoInput() {
+  const input = document.querySelector('.display')
+  input.style.border = "solid 1px white"
+
+  setTimeout(() => {
+    input.style.border = 'none'
+  }, "1000");
+}
